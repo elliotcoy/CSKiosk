93e3a187172f47969a1282245af9aee35238ef2b
elliotcoy <elliotcoy@gmail.com>
2016-02-26 11:07:34 -0400

    Working on Slide Show

--- slideShow.js
+++ slideShow.js
@@ -1,15 +1,18 @@
 var expand = 1;
-
-function slideShowInit() {
-    //ONLOAD
-    setTimeout(slideShow, 500);
-}
+var slide = 0;
 
 function slideShow() {
-    //THIS IS WHERE THE SLIDES CHANGE (every 5 seconds)
-    $("#slideShow").animate({
-        opacity: '-=0.1'
-    });
+    if (slide) {
+        $("#slide2").animate({
+            opacity: '=0.0'
+        });
+    }
+    else {
+        $("#slide2").animate({
+            opacity: '=1.0'
+        });
+    }
+    slide = !slide;
     setTimeout(slideShow, 5000);
 }
 
@@ -21,13 +24,20 @@ function expandSlideShow() {
             width: '100%',
             height: '100%'
         });
+        $("#exit").animate({
+            opacity: '0.0'
+        });
+
     }
     else{
         $("#slideShow").animate({
             top: '10px',
-            right: '15%',
-            width: '65px',
+            right: '14%',
+            width: '98px',
             height: '55px'
+        });
+        $("#exit").animate({
+            opacity: '1.0'
         });
     }
     expand = !expand;
