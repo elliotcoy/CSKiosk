579adce8d2a342fd3d4f5f48b4a153cf3f7ebf4c
RBeaulieu <beaulieu_raph@outlook.com>
2016-02-26 08:58:15 -0400

    Titles and stuff

--- content.js
+++ content.js
@@ -1,33 +1,60 @@
-var topic0 = "";
+var topic0 =	"<h1><b>Welcome!</b></h1><br>" +
+				"Welcome to the Computer Science Information Guide!<br>" +
+				"<br>" +
+				"To get started, click on any of the links to left.";
 
-var topic1 = "<br>topic1<br><br>topic1";
+var topic1 =	"<h1><b>Computer Science!</b></h1><br>";
 
-var topic2 = "<br>topic2<br><br>topic2";
+var topic2 =	"<h1><b>Myths About Computer Science</b></h1><br>" +
+				"Despite its impact on modern day life, many people have misconceptions about what computer science<br>" +
+				"is actually about. Here's a list of common misconceptions, and why they're untrue.<br>" +
+				"<ul>" +
+				"<li>Computer science is about being able to use computer programs</li>" +
+				"<li style='margin-left:2em'>test</li>" +
+				"<li>Computer science is about programming</li>" +
+				"<li style='margin-left:2em'>test</li>" +
+				"<li>Computer science is about computer repair and IT</li>" +
+				"<li style='margin-left:2em'>test</li>" +
+				"<li>Computer science sounds boring</li>" +
+				"<li style='margin-left:2em'>test</li>" +
+				"<li>Computer science is for geeks</li>" +
+				"<li style='margin-left:2em'>test</li>" +
+				"<li>Computer science lacks creativity</li>" +
+				"<li style='margin-left:2em'>test</li>" +
+				"<li>Computer science is not for women</li>" +
+				"<li style='margin-left:2em'>test</li>" +
+				"</ul>";
 
-var topic3 = "<br>topic3<br><br>topic3";
+var topic3 =	"<h1><b>What is Computer Science?</b></h1><br>";
 
-var topic4 = "<br>topic4<br><br>topic4";
+var topic4 =	"<h1><b>History of Computer Science</b></h1><br>";
 
-var topic5 = "<br>topic5<br><br>topic5";
+var topic5 =	"<h1><b>Topics in Computer Science</b></h1><br>";
 
-var topic6 = "<br>topic6<br><br>topic6";
+var topic6 =	"<h1><b>They're Everywhere!</b></h1><br>";
 
-var topic7 = "<br>topic7<br><br>topic7";
+var topic7 =	"<h1><b>Types of Jobs</b></h1><br>";
 
-var topic8 = "<br>topic8<br><br>topic8";
+var topic8 =	"<h1><b>Employers</b></h1><br>";
 
-var topic9 = "<br>topic9<br><br>topic9";
+var topic9 =	"<h1><b>Demand</b></h1><br>";
 
-var topic10 = "<br>topic10<br><br>topic10";
+var topic10 =	"<h1><b>Faculty of Computer Science</b></h1><br>";
 
-var topic11 = "<br>topic11<br><br>topic11";
+var topic11 =	"<h1><b>First Year Schedule</b></h1><br>";
 
-var topic12 = "<br>topic12<br><br>topic12";
+var topic12 =	"<h1><b>Co-Op Program</b></h1><br>";
 
-var topic13 = "<br>topic13<br><br>topic13";
+var topic13 =	"<h1><b></b>Concurrent Programs</h1><br>";
 
-var topic14 = "<br>topic14<br><br>topic14";
+var topic14 =	"<h1><b>And Beyond!</b></h1><br>";
 
-var topic15 = "<br>topic15<br><br>topic15";
+var topic15 =	"<h1><b>Applying to UNB</b></h1><br>" +
+				"If you find yourself interested in computer science, consider applying to become a student at UNB<br>" +
+				"and join our faculty! You can apply any time you'd like; we're always looking for prospective<br>" +
+				"students! However, the best time to apply is before March 31st, as this ensures the best possible<br>" +
+				"chance of getting in for the next academic school year.<br>" +
+				"<br>" +
+				"To get started, on a computer or mobile device, navigate to <b><i>apply.unb.ca</i></b> and start your application!";
 
-var topic16 = "<br>topic16<br><br>topic16";
+var topic16 =	"<h1><b>Requesting More Information</b></h1><br>";
