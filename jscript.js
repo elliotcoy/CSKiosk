f3afe539c0050b2b6ece2f06c901142978ba2be8
elliotcoy <elliotcoy@gmail.com>
2016-02-25 23:55:47 -0400

    Nav Menu 1 Working
    Needs to change content still

--- jscript.js
+++ jscript.js
@@ -1,12 +1,12 @@
 function ComputerWhat() {
-    $("div.nav1").animate({
+    $(".nav1").animate({
         left: '-14%'
     });
-    $("div.invisButton").animate({
+    $(".invisButton").animate({
         left: '0px',
-        width: '6%'
+        width: '6%',
+        height: '100%'
     });
-
     $("#ComputerWhatNav").animate({
         left: '6%'
     });
@@ -19,21 +19,97 @@ function ComputerWhat() {
     $("#ApplyNav").animate({
         left: '-8%'
     });
+
+    $("#ComputerWhat").addClass("navBtn1Selected").removeClass("navBtn1");
 }
 
-function expandNav() {
-    $("div.invisButton").click(function () {
-        $("div.nav1").animate({
-            left: '0px'
-        });
-        $("div.invisButton").animate({
-            left: '0px',
-            width: '0%'
-        });
+function Careers() {
+    $(".nav1").animate({
+        left: '-14%'
+    });
+    $(".invisButton").animate({
+        left: '0px',
+        width: '6%',
+        height: '100%'
+    });
+    $("#ComputerWhatNav").animate({
+        left: '-8%'
+    });
+    $("#CareersNav").animate({
+        left: '6%'
+    });
+    $("#UNBcsNav").animate({
+        left: '-8%'
+    });
+    $("#ApplyNav").animate({
+        left: '-8%'
     });
 
+    $("#Careers").addClass("navBtn1Selected").removeClass("navBtn1");
+}
+
+function UNBcs() {
+    $(".nav1").animate({
+        left: '-14%'
+    });
+    $(".invisButton").animate({
+        left: '0px',
+        width: '6%',
+        height: '100%'
+    });
     $("#ComputerWhatNav").animate({
+        left: '-8%'
+    });
+    $("#CareersNav").animate({
+        left: '-8%'
+    });
+    $("#UNBcsNav").animate({
         left: '6%'
+    });
+    $("#ApplyNav").animate({
+        left: '-8%'
+    });
+
+    $("#UNBcs").addClass("navBtn1Selected").removeClass("navBtn1");
+}
+
+function Apply() {
+    $(".nav1").animate({
+        left: '-14%'
+    });
+    $(".invisButton").animate({
+        left: '0px',
+        width: '6%',
+        height: '100%'
+    });
+    $("#ComputerWhatNav").animate({
+        left: '-8%'
+    });
+    $("#CareersNav").animate({
+        left: '-8%'
+    });
+    $("#UNBcsNav").animate({
+        left: '-8%'
+    });
+    $("#ApplyNav").animate({
+        left: '6%'
+    });
+
+    $("#Apply").addClass("navBtn1Selected").removeClass("navBtn1");
+}
+
+
+function expandNav() {
+    $(".nav1").animate({
+        left: '0px'
+    });
+    $(".invisButton").animate({
+        left: '0px',
+        width: '0%',
+        height: '100%'
+    });
+    $("#ComputerWhatNav").animate({
+        left: '-8%'
     });
     $("#CareersNav").animate({
         left: '-8%'
@@ -44,6 +120,11 @@ function expandNav() {
     $("#ApplyNav").animate({
         left: '-8%'
     });
+
+    $("#ComputerWhat").addClass("navBtn1").removeClass("navBtn1Selected");
+    $("#Careers").addClass("navBtn1").removeClass("navBtn1Selected");
+    $("#UNBcs").addClass("navBtn1").removeClass("navBtn1Selected");
+    $("#Apply").addClass("navBtn1").removeClass("navBtn1Selected");
 }
 
 function pHolder() {
